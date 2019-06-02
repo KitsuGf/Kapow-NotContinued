ec148ca21c6129bc79b427dc73b678b1dab2b00a
KitsuGf <jmontesmart@gmail.com>
2019-06-02 14:22:31 +0200

    Add the correct commented lines

--- Kapow!/src/common/CommonData.java
+++ Kapow!/src/common/CommonData.java
@@ -9,30 +9,38 @@ import java.sql.Date;
 
 /**
  * Class of Common data between Archive and User.
- * This class have the atributes: Name and Date shared with Archive and user.
+ * This class have the atributes: Name and Date shared with Archive and User.
  * @author Kitsu.
  */
+
 public class CommonData {
+	//Vars of the constructor
     String name;
     Date dateUser;
-
+    
+    //Data for constructor
     public CommonData(String name, Date dateUser) {
         this.name = name;
         this.dateUser = dateUser;
     }
     
+    //Get the date from the user, this one is reutiliced for 
+    //get the date from the comic
     public Date getDateUser() {
 		return dateUser;
 	}
-
+    
+    //Set the date from the user, this one is reutiliced for 
+    //set the date from the comic
 	public void setDateUser(Date dateUser) {
 		this.dateUser = dateUser;
 	}
-
+	
+	//Get the name, comic or name of the user
     public String getName() {
         return name;
     }
-
+    //Set the name, comic or name of the user
     public void setName(String name) {
         this.name = name;
     }
